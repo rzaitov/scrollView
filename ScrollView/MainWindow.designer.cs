1984bea391f6541c929f5e2a580657c8c57c7a95
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-10-07 19:29:30 +0400

    place tmp button

--- ScrollView/MainWindow.designer.cs
+++ ScrollView/MainWindow.designer.cs
@@ -1,7 +1,26 @@
-﻿namespace ScrollView
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ScrollView
 {
-	[global::Foundation.Register ("MainWindow")]
-	public partial class MainWindow
+	[Register ("MainWindow")]
+	partial class MainWindow
 	{
+		[Outlet]
+		AppKit.NSButton Btn { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (Btn != null) {
+				Btn.Dispose ();
+				Btn = null;
+			}
+		}
 	}
 }
